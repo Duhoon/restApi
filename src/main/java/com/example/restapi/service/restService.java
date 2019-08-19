429de69bfecd11b4e09a3b8c5c340c0f76c32a9d
Duhoon <eclip6@naver.com>
2019-08-19 14:14:38 +0900

    add

--- src/main/java/com/example/restapi/service/restService.java
+++ src/main/java/com/example/restapi/service/restService.java
@@ -1,28 +1,20 @@
-package com.example.restapi.controller;
+package com.example.restapi.service;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@CrossOrigin
-@RestController
-
-public class restDaily {
-    @RequestMapping(value = "/daily")
-    public Object actionDaily(){
+@Service
+public class restService {
+    public Object actionMethod(){
         RestTemplate restTemplate = new RestTemplate();
         Object tempObject = new Object();
         String apiID = "a91a40ff6f14cc255d72ec7689dce371";
         String targetUrl = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid="+apiID;
         Map<String, Object> resultObject = new HashMap<String, Object>();
         resultObject = (Map<String, Object>) restTemplate.getForObject(targetUrl, Object.class);
-        ArrayList<Object> timelist = new ArrayList<Object>();
-        timelist = (ArrayList<Object>) resultObject.get("list");
-        return timelist;
+        return resultObject;
     }
 }
---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,26 @@
+$(function(){
+    $.get("http://localhost:8080/weather", function(data){
+        //three hours work
+        $.each(data, function(index, item){
+            if(index == 8){
+                return false;
+            }
+            var weather = item.weather[0];
+            $("#date").append("<th>" + item.dt_txt + "</th>");
+            var iconurl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+            $("#icon").append("<td><img src=\""+ iconurl + "\" "+ "</td>");
+            $("#weather").append("<td>" + weather.main + "</td>");
+            $("#climate").append("<td>" + item.main.temp + "</td>");
+        });
+        $.each(data, function(index, item){
+            if(item.dt_txt.substr(11,18) == "12:00:00"){
+                var weather = item.weather[0];
+                $("#dl_date").append("<th>" + item.dt_txt.substr(0,10) + "</th>");
+                var iconurl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+                $("#dl_icon").append("<td><img src=\""+ iconurl + "\" "+ "</td>");
+                $("#dl_weather").append("<td>" + weather.main + "</td>");
+                $("#dl_climate").append("<td>" + item.main.temp + "</td>");
+            }
+        });
+    });
+})
